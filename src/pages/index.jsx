9167f84dd84c985c96d348a788872d79a964b646
Edward Ha <admin@MacBook-Pro-cua-Admin.local>
2024-03-21 16:02:44 +0700

    feature update home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <>
+    <div style={{ backgroundColor: '#E1F0DA', height: '100vh', width: '100vw' }} >
       {/* <NextSeo
         title="Professional Cleaning Services in Singapore | KungFu Helper"
         description="Welcome to KungFu Helper, the leading provider of professional cleaning services in Singapore. We offer a wide range of cleaning solutions for homes and offices. Contact us for reliable and efficient cleaning service."
       />
       <HomeComponent /> */}
-    </>
+    </div>
   );
 };
 
