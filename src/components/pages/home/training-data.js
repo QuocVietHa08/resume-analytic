adebe63a2c7be41b1b68bc93770073d91283f5ef
Edward Ha <quocvietha08@gmail.com>
2024-03-30 13:43:07 +0700

    Feature add ai

--- src/components/pages/home/training-data.js
+++ src/components/pages/home/training-data.js
@@ -4,3 +4,22 @@ export const ANSWER_FORMAT = 'Base on the resume data. You need to provide a cle
 export const FINAL_PRMOPT = ''
 export const BEGIN_PROMPT= 'Human: Act as a senior programmer who really good at interview other programer. I will give you a resume and you need to analyze that resume:'
 
+export const PROMPT_GET_SALARAY = `Human: Act as a senior programmer who really good at interview other programer. I will give you a resume and you need to analyze that resume.
+Just return the salary of the resume.
+Don't return any other information. Don't return the salary in a sentence. Just return the number.
+For example: 1000. Here is the information of the resume:`
+
+export const PROMPT_GET_KEY_FACTOR = `Human: Act as a senior programmer who really good at interview other programer. I will give you a resume and you need to analyze that resume.
+Then return 5 key factors of this resume.
+Return in buttlet point.
+Here is the information of the resume:`
+
+export const PROMPT_GET_IMPROVEMENT = `Human: Act as a senior programmer who really good at interview other programer. I will give you a resume and you need to analyze that resume.
+Then return 5 key improvement of this resume.
+Return in buttlet point.
+Here is the information of the resume:`
+
+export const PROMPT_ANALYZE_RESUME = `Human: Act as a senior programmer who really good at interview other programer. I will give you a resume and you need to analyze that resume.
+Then return 3 key improvement and 3 key feature and estimate salary of this resume.
+Return in buttlet point, each point in one line with about 10 words each line.
+Here is the information of the resume:`
