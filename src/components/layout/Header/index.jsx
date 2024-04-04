ef8f58ab3ba0871208a0b35312cd961ee556ebc7
Edward Ha <quocvietha08@gmail.com>
2024-04-04 16:19:39 +0700

    Feature add resume page

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,4 +1,4 @@
-// import Link from 'next/link';
+import Link from 'next/link';
 import styles from './styles.module.scss';
 
 const Header = () => {
@@ -7,9 +7,12 @@ const Header = () => {
       <div className={styles.headerWrapper}>
         <div className={styles.logoWrapper}></div>
         <div className="flex items-center gap-20">
-          {/* <Link href="/resume-builder" className={styles.buttonLogin} rel="noreferrer">
-            Resume-builder
-          </Link> */}
+          <Link href="/" className={styles.buttonLogin} rel="noreferrer">
+            Home
+          </Link>
+          <Link href="/resume" className={styles.buttonLogin} rel="noreferrer">
+            Resume
+          </Link>
           <a target="_blank" href="https://github.com/QuocVietHa08/resume-analytic" className={styles.buttonLogin} rel="noreferrer">
             Repository
           </a>
---
+++ src/components/pages/resume/index.jsx
@@ -0,0 +1,91 @@
+/* eslint-disable react/jsx-boolean-value */
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-array-constructor */
+import { useState, useRef, useEffect } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, PointMaterial } from '@react-three/drei';
+import { random } from 'maath';
+
+import styles from './styles.module.scss';
+
+// const LinkedinIcon = () => {
+//   return (
+//     <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+//       <path
+//         d="M44.4567 0H3.54333C2.60358 0 1.70232 0.373315 1.03782 1.03782C0.373315 1.70232 0 2.60358 0 3.54333V44.4567C0 45.3964 0.373315 46.2977 1.03782 46.9622C1.70232 47.6267 2.60358 48 3.54333 48H44.4567C45.3964 48 46.2977 47.6267 46.9622 46.9622C47.6267 46.2977 48 45.3964 48 44.4567V3.54333C48 2.60358 47.6267 1.70232 46.9622 1.03782C46.2977 0.373315 45.3964 0 44.4567 0ZM14.3067 40.89H7.09V17.9667H14.3067V40.89ZM10.6933 14.79C9.87473 14.7854 9.07583 14.5384 8.39747 14.0802C7.71911 13.622 7.19168 12.9731 6.88175 12.2154C6.57183 11.4577 6.4933 10.6252 6.65606 9.82291C6.81883 9.02063 7.2156 8.28455 7.79631 7.70756C8.37702 7.13057 9.11563 6.73853 9.91893 6.58092C10.7222 6.42331 11.5542 6.50719 12.3099 6.82197C13.0656 7.13675 13.7111 7.66833 14.1649 8.34962C14.6188 9.03092 14.8606 9.83138 14.86 10.65C14.8677 11.1981 14.765 11.7421 14.558 12.2496C14.351 12.7571 14.044 13.2178 13.6551 13.6041C13.2663 13.9905 12.8037 14.2946 12.2948 14.4983C11.786 14.702 11.2413 14.8012 10.6933 14.79ZM40.9067 40.91H33.6933V28.3867C33.6933 24.6933 32.1233 23.5533 30.0967 23.5533C27.9567 23.5533 25.8567 25.1667 25.8567 28.48V40.91H18.64V17.9833H25.58V21.16H25.6733C26.37 19.75 28.81 17.34 32.5333 17.34C36.56 17.34 40.91 19.73 40.91 26.73L40.9067 40.91Z"
+//         fill="#0A66C2"
+//       />
+//     </svg>
+//   );
+// };
+
+// const GithubIcon = () => {
+//   return (
+//     <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+//       <g clipPath="url(#clip0_910_44)">
+//         <path
+//           fillRule="evenodd"
+//           clipule="evenodd"
+//           d="M24.0199 0C10.7375 0 0 10.8167 0 24.1983C0 34.895 6.87988 43.9495 16.4241 47.1542C17.6174 47.3951 18.0545 46.6335 18.0545 45.9929C18.0545 45.4319 18.0151 43.509 18.0151 41.5055C11.3334 42.948 9.94198 38.6209 9.94198 38.6209C8.86818 35.8164 7.27715 35.0956 7.27715 35.0956C5.09022 33.6132 7.43645 33.6132 7.43645 33.6132C9.86233 33.7735 11.1353 36.0971 11.1353 36.0971C13.2824 39.7827 16.7422 38.7413 18.1341 38.1002C18.3328 36.5377 18.9695 35.456 19.6455 34.8552C14.3163 34.2942 8.70937 32.211 8.70937 22.9161C8.70937 20.2719 9.66321 18.1086 11.1746 16.4261C10.9361 15.8253 10.1008 13.3409 11.4135 10.0157C11.4135 10.0157 13.4417 9.3746 18.0146 12.4996C19.9725 11.9699 21.9916 11.7005 24.0199 11.6982C26.048 11.6982 28.1154 11.979 30.0246 12.4996C34.5981 9.3746 36.6262 10.0157 36.6262 10.0157C37.9389 13.3409 37.1031 15.8253 36.8646 16.4261C38.4158 18.1086 39.3303 20.2719 39.3303 22.9161C39.3303 32.211 33.7234 34.2539 28.3544 34.8552C29.2296 35.6163 29.9848 37.0583 29.9848 39.3421C29.9848 42.5871 29.9454 45.1915 29.9454 45.9924C29.9454 46.6335 30.383 47.3951 31.5758 47.1547C41.12 43.9491 47.9999 34.895 47.9999 24.1983C48.0392 10.8167 37.2624 0 24.0199 0Z"
+//           fill="white"
+//         />
+//       </g>
+//       <defs>
+//         <clipPath id="clip0_910_44">
+//           <rect width="48" height="48" fill="white" />
+//         </clipPath>
+//       </defs>
+//     </svg>
+//   );
+// };
+
+const Stars = (props) => {
+  const ref = useRef();
+  const [sphere, setSphere] = useState();
+  useEffect(() => {
+    setSphere(random.inSphere(new Float32Array(5000), { radius: 1.5 }));
+  }, []);
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
+        <PointMaterial transparent color="#ffa0e0" size={0.005} sizeAttenuation={true} depthWrite={false} />
+      </Points>
+    </group>
+  );
+};
+
+const Layout = () => {
+  return (
+    <Canvas camera={{ position: [0, 0, 1] }}>
+      <Stars />
+    </Canvas>
+  );
+};
+const Overlay = () => {
+  return (
+    <div className={styles.overlayContainer}>
+      <div className={styles.overlayWrapper}>
+        <h1>Hi, I'm Edward</h1>
+      </div>
+    </div>
+  );
+};
+
+const Resume = () => {
+  return (
+    <div className={styles.container}>
+      <Layout />
+      <Overlay />
+    </div>
+  );
+};
+
+export default Resume;
