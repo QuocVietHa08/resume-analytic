3644cb1b935648f1f249d8d7c5b614eac9264c21
Edward Ha <quocvietha08@gmail.com>
2024-04-07 23:52:09 +0700

    Feature update logic v4

--- src/components/pages/resume-builder/Achivement.jsx
+++ src/components/pages/resume-builder/Achivement.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Input, Button, Modal } from 'antd';
+import { EditOutlined, MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+
 import styles from './styles.module.scss';
 
-const Achivement = ({ info }) => {
+const Achivement = ({ info, onChangeInfo }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
+
+  if (info.length === 0) return null;
+
   return (
     <div className={styles.achivement}>
+      <ModalEditPersonalProject
+        info={info}
+        onSubmit={onChangeInfo}
+        onOpenModal={handleOpenModal}
+        onCloseModal={handleCloseModal}
+        open={open}
+      />
       <div className={styles.title}>
         <div className={styles.text}>Achivements</div>
         <div className={styles.line}></div>
@@ -25,8 +47,142 @@ const AchivementItem = ({ info }) => {
       <span className="text-bold">{info.name}: </span>
       <span className={styles.description}>
         {info.description}
-        <span className="ml-5 text-bold">({info.startDate}-{info.endDate})</span>
+        <span className="ml-5 text-bold">
+          ({info.startDate}-{info.endDate})
         </span>
+      </span>
     </div>
+  );
+};
+
+const ModalEditPersonalProject = ({ info, open, onOpenModal, onCloseModal, onSubmit }) => {
+  const [form] = Form.useForm();
+  const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+
+  const handleFinish = (values) => {
+    const { achivement } = values;
+    onSubmit(achivement, 'achivement');
+    onCloseModal();
+  };
+
+  const handleCancel = () => {
+    onCloseModal();
+    form.resetFields();
+  };
+
+  const initialValues = {
+    achivement: info.map((record, index) => ({
+      ...record,
+      key: index,
+    })),
+  };
+
+  return (
+    <>
+      <button type="button" className={styles.buttonEdit} onClick={onOpenModal} aria-label="Edit">
+        <EditOutlined />
+      </button>
+      <Modal width={1000} closeIcon={<></>} title="Edit Achivements" open={open} footer={null}>
+        <Form {...layout} form={form} initialValues={initialValues} onFinish={handleFinish}>
+          <Form.List name="achivement">
+            {(fields, { add, remove }) => (
+              <>
+                {fields.map(({ key, name, ...restField }) => (
+                  <div key={key} className={styles.formListItem}>
+                    <Form.Item
+                      {...restField}
+                      name={[name, 'name']}
+                      label="Project Name"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Please input your achivement!',
+                        },
+                      ]}
+                    >
+                      <Input />
+                    </Form.Item>
+                    <Form.Item
+                      {...restField}
+                      name={[name, 'description']}
+                      label="Project Description"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Please input your achivement description!',
+                        },
+                      ]}
+                    >
+                      <Input.TextArea rows={3} />
+                    </Form.Item>
+                    <Form.Item
+                      {...restField}
+                      name={[name, 'startDate']}
+                      label="Start Date"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Please input your achivement start date!',
+                        },
+                      ]}
+                    >
+                      <Input />
+                    </Form.Item>
+                    <Form.Item
+                      {...restField}
+                      name={[name, 'endDate']}
+                      label="End Date"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Please input your achivement end date!',
+                        },
+                      ]}
+                    >
+                      <Input />
+                    </Form.Item>
+
+                    <button type="button" className={styles.formListItemDeleteIcon} onClick={() => remove(name)} aria-label="Remove">
+                      <MinusCircleOutlined />
+                    </button>
+                  </div>
+                ))}
+                <Form.Item
+                  wrapperCol={{
+                    offset: 0,
+                    span: 24,
+                  }}
+                  className="mt-10"
+                >
+                  <Button type="dashed" className="mx-center w-full" onClick={() => add()} block icon={<PlusOutlined />}>
+                    Add Project
+                  </Button>
+                </Form.Item>
+              </>
+            )}
+          </Form.List>
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+            <Button className="ml-10" onClick={handleCancel}>
+              Close
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
   );
 };
